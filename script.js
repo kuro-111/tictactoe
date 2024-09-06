3b15325ad80165e72baec7e7ef26231bcfc20d2f
graceann <graceann@XUbuntu.myguest.virtualbox.org>
2024-09-06 09:02:41 +0800

    Set up plans and get started with some functions

---
+++ script.js
@@ -0,0 +1,13 @@
+const playButton = document.getElementsByClassName("play");
+
+
+
+console.log(playButton);
+//Make the players function
+//Make game board function
+
+function gamePlayers() {
+
+
+
+}
