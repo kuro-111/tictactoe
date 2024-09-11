da2bbefac6aeb8b52f5379f37be8da05d0aaaa8a
graceann <graceann@XUbuntu.myguest.virtualbox.org>
2024-09-11 10:09:55 +0800

    Make the game board function

--- script.js
+++ script.js
@@ -6,7 +6,21 @@ console.log(playButton);
 //Make the players function
 //Make game board function
 
-function gamePlayers() {
+function gameBoard(event) {
+
+    let boardGrid;
+
+    for(i = 0; i < 3; i++){
+        let boardGridRow = document.createElement("div");
+        
+        for(j = 0; j < 3; j++){
+            //make column
+        }
+    }
+    //finish making the board and incorporate a player function
+}
+
+function gamePlayers(game) {
 
 
 
