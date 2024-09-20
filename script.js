dd84258e84eafb93eebee9724e754fb44feed65c
graceann <graceann@XUbuntu.myguest.virtualbox.org>
2024-09-20 08:59:21 +0800

    Figure out the game logic

--- script.js
+++ script.js
@@ -7,32 +7,40 @@ console.log(playButton);
 //Make game board function
 
 function createGameBoard() {
+  //MAKE THE GAME IN THE CONSOLE
+  //use `` for whitespace
+  //finish making the board and incorporate a player function
+  let gameBoard = [null, null, null, null, null, null, null, null, null];
+  // Make sure to put this code above the return cutie
+  // Or it no worky
+  let name = prompt("Enter player name", "Mario");
+  let piece = prompt("x or O");
+  let score = 0;
 
-    //MAKE THE GAME IN THE CONSOLE
-    //use `` for whitespace
-    //finish making the board and incorporate a player function
-    return [
-        null, null, null,
-        null, null, null,
-        null, null, null
-    ]
-    // Make sure to put this code above the return cutie
-    // Or it no worky
-        let player = prompt("Enter player name", "Mario");
-         //set condition to not acccept null
+  while (name == null) {
+      name = prompt("Enter player name", "Mario");
+  }
 
+  while (piece != "X" && piece != "O") {
+     piece = prompt("X or O");
+  }
+  
 
+  createPlayer(name, piece, score);
+
+  //set condition to not acccept null
 }
 
 createGameBoard();
 
-
 function createPlayer(name, piece, score) {
-    return {
-        name,
-        piece,
-        score
-    }
+
+
+  return {
+    name,
+    piece,
+    score,
+  };
 }
 
 // let boardGrid = document.createElement("div");
@@ -42,7 +50,7 @@ function createPlayer(name, piece, score) {
 //     let boardGridRow = document.createElement("div");
 
 //     boardGrid.appendChild(boardGridRow);
-    
+
 //     for(j = 0; j < 3; j++){
 //         let boardGridColumn = document.createElement("div");
 //     }
